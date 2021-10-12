a49e92aad04376a357a39a22323a61bb1526ade1
elfaro1453 <54815985+elfaro1453@users.noreply.github.com>
2021-10-12 09:32:16 +0700

    update todo item

--- 04-React/project/01-todo-app/src/components/TodoItem.jsx
+++ 04-React/project/01-todo-app/src/components/TodoItem.jsx
@@ -4,7 +4,10 @@ import './styles/TodoItem.css'
 function TodoItem(props) {
   let data = props.data
   let listItem = data.map((item) => {
-      return <li className="itemlist" key={item.id}>{item.title}</li>
+      return <li className="itemlist" key={item.id}>
+        <input type="checkbox" checked={item.completed}/>
+        {item.title}
+        </li>
   })
 
   return (
