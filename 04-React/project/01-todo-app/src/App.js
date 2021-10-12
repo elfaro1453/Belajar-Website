fba36d2e2191feadd3889dbef2a25dfb38dcc382
elfaro1453 <54815985+elfaro1453@users.noreply.github.com>
2021-10-12 08:37:25 +0700

    push

--- 04-React/project/01-todo-app/src/App.js
+++ 04-React/project/01-todo-app/src/App.js
@@ -3,6 +3,26 @@ import AddTodo from "./components/AddTodo";
 import Header from "./components/Header";
 import Todos from "./components/Todos";
 
+var state = {
+  todos: [
+    {
+    id: 1,
+    title: "Setup development environment",
+    completed: true
+    },
+    {
+    id: 2,
+    title: "Develop website and add content",
+    completed: false
+  },
+  {
+  id: 3,
+  title: "Deploy to live server",
+  completed: false
+  }
+  ]
+ };
+
 function App() {
   return (
     <React.Fragment>
